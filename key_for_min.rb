7b659d78ac58937b74fef897a84b20489149458a
Samantha Sanford <sanfordsamantha@icloud.com>
2020-03-27 01:46:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,5 +6,12 @@ def key_for_min_value(name_hash)
   min_key = nil
   
   name_hash.each do |key, value|
+    if min_value == nil || value < min_value 
+      min_value = value
+      min_key = key
+    end
+  end
+  
+  min_key
 
 end
